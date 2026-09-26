277cc507b5f184e7bc75114b0c74a4e0f5f0fb2d
suryabhushankumar562-a11y <suryabhushankumar562@gmail.com>
2026-09-26 11:37:47 +0530

    Fix PostgreSQL database configuration

--- PoliticalPartyManagement/settings.py
+++ PoliticalPartyManagement/settings.py
@@ -42,6 +42,10 @@ ALLOWED_HOSTS = os.environ.get(
     "127.0.0.1,localhost"
 ).split(",")
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
